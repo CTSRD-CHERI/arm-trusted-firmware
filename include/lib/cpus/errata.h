96afdbf34e7249e9527ba4337bf6d230ac378c3c
Ruslan Bukin <br@freebsd.org>
2023-12-19 16:30:36 +0000

    cleanup

--- include/lib/cpus/errata.h
+++ include/lib/cpus/errata.h
@@ -19,11 +19,17 @@
 
 #define ERRATUM_WA_FUNC		0
 #define ERRATUM_CHECK_FUNC	ERRATUM_WA_FUNC + ERRATUM_WA_FUNC_SIZE
-#define ERRATUM_ID		ERRATUM_CHECK_FUNC + ERRATUM_CHECK_FUNC_SIZE + 0
+#define ERRATUM_ID		ERRATUM_CHECK_FUNC + ERRATUM_CHECK_FUNC_SIZE
 #define ERRATUM_CVE		ERRATUM_ID + ERRATUM_ID_SIZE
 #define ERRATUM_CHOSEN		ERRATUM_CVE + ERRATUM_CVE_SIZE
 #define ERRATUM_MITIGATED	ERRATUM_CHOSEN + ERRATUM_CHOSEN_SIZE
-#define ERRATUM_ENTRY_SIZE	ERRATUM_MITIGATED + ERRATUM_MITIGATED_SIZE + 8
+#define ERRATUM_ENTRY_SIZE_UNA	ERRATUM_MITIGATED + ERRATUM_MITIGATED_SIZE
+
+#if CPU_WORD_SIZE == 16
+#define	ERRATUM_ENTRY_SIZE	ERRATUM_ENTRY_SIZE_UNA + 8
+#else
+#define	ERRATUM_ENTRY_SIZE	ERRATUM_ENTRY_SIZE_UNA
+#endif
 
 #ifndef __ASSEMBLER__
 #include <lib/cassert.h>
@@ -45,11 +51,10 @@ struct erratum_entry {
 	uint8_t chosen;
 	/* TODO(errata ABI): placeholder for the mitigated field */
 	uint8_t _mitigated;
-} __aligned(16);
+} __aligned(STRUCT_ALIGN);
 
 CASSERT(sizeof(struct erratum_entry) == ERRATUM_ENTRY_SIZE,
 	assert_erratum_entry_asm_c_different_sizes);
-
 #else
 
 /*
